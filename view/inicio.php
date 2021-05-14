f5c408476ec81c8c1d2b442d5b2ae8b7ba0d6687
Posi-10 <64515605+Posi-10@users.noreply.github.com>
2021-05-14 11:29:11 -0500

    Initial commit

--- view/inicio.php
+++ view/inicio.php
@@ -1,7 +1,8 @@
 <div class="container">
   <div class="row justify-content-center mt-3">
     <div class="col-sm-5 mt-3">
-      <span class="btn btn-outline-info btn-block" id="btn_informacion">Informacion de la sección</span>
+      <!-- <span class="btn btn-outline-info btn-block" id="btn_informacion">Informacion de la sección</span> -->
+      <h1 class="display-1">Hola Bienvenido</h1>
     </div>
   </div>
   <div class="row justify-content-center mt-3">
